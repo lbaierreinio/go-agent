b6c3a61d99789c6ae47a7a0c6b87d50480f6e5dc
LucBaierReinio <luc.baier-reinio@mail.mcgill.ca>
2022-11-16 21:34:32 -0500

    add basic MCTS node class & function to determine if can move from one cell to the next

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,6 +1,7 @@
 # Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
+from copy import deepcopy
 import sys
 
 
@@ -11,6 +12,7 @@ class StudentAgent(Agent):
     add any helper functionalities needed for your agent.
     """
 
+
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
@@ -20,6 +22,35 @@ class StudentAgent(Agent):
             "d": 2,
             "l": 3,
         }
+
+    class MCTSNode: 
+        def __init__(self, parent, my_pos, adv_pos, max_step, chess_board, move, my_turn):
+            self.parent = parent
+            self.chess_board = chess_board
+            self.move = move
+            self.times_visited = 0
+            self.win_count = 0
+            self.children = []
+            self.my_pos = my_pos
+            self.adv_pos = adv_pos
+            self.max_step = max_step
+            self.my_turn = my_turn
+            self.dir_map = {
+            "u": 0,
+            "r": 1,
+            "d": 2,
+            "l": 3,
+            }
+            
+        
+        """
+        Determine if can take one step from current position to the cell in direction 'direction'
+        """
+        def canMove(self, current_position, direction):
+            # Check if we can move in direction from current_position by checking that it is 'clear' and opponent is not there. 
+            return (not self.chess_board[current_position[0], current_position[1], self.dir_map[direction]] 
+            and (current_position[0] != self.adv_pos[0] and current_position[1] != self.adv_pos[1]))
+            
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -38,3 +69,8 @@ class StudentAgent(Agent):
         """
         # dummy return
         return my_pos, self.dir_map["u"]
+
+   
+
+
+
