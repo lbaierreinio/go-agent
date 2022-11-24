a7a9dd8900a35492fdafc318c92880070850de3f
matt-macd <mattmacdonald122@gmail.com>
2022-11-24 01:19:28 -0500

    add test bat file and xlsx to export data

---
+++ run_tests.bat
@@ -0,0 +1,13 @@
+c:\
+cd C:\Users\mattm\Documents\Classes\COMP 424\424_final
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 2
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 3
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 4
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 5
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 6
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 7
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 8
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 9
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 10
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 11
+start /wait python simulator.py --player_1 student_agent --player_2 student_agent --autoplay --autoplay_runs 40 --simulations_per_expanded_child 12
